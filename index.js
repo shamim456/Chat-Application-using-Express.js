3d93b86bb6d3f7cdc857737a9ae869783b153584
shamim <shamimhossen3912@gmail.com>
2023-03-14 23:08:39 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// external import
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const app = express();
+dotenv.config();
+
+// database connection
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, { useNewUrlParser: true })
+  .then(() => console.log("database connected"))
+  .catch((err) => console.log(err));
+
+app.listen(process.env.PORT, () => {
+  console.log("App is running properly");
+});
+
+// request parser
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// set view engine
+
+app.set("view engine", "ejs");
+
+// set static folder
+
+app.use(express.static(path.join(__dirname, "Public")));
